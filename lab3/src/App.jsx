d09b7241b30dd184ee455c2e9dc2c27f87842a40
Slava <vt221_psa@student.ztu.edu.ua>
2024-12-21 13:00:38 +0200

    Integrate ToorSearchForm into App

--- lab3/src/App.jsx
+++ lab3/src/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import TourSearchForm from '../src/copmonents/TourSearchForm';
 
 const App = () => (
   <div>
+    <h1>Пошук туру</h1>
+    <TourSearchForm />
   </div>
 );
 
