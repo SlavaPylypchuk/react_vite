8fcebd2e59943402c2bf420cdefe59b4006fa388
Slava <vt221_psa@student.ztu.edu.ua>
2024-12-21 12:43:58 +0200

    Initial commit

---
+++ lab1/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ChessClock from './components/ChessClock/ChessClock';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
