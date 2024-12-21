f2c0a29700a80b5ac8d781abdb1a1cc0525baac0
Slava <vt221_psa@student.ztu.edu.ua>
2024-12-21 12:56:03 +0200

    Integrate WebSocketComponent into App

--- lab2/src/App.jsx
+++ lab2/src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import WebSocketComponent from './components/WebSocket/WebSocketComponent';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <h1>Веб сокет</h1>
+      <WebSocketComponent /> {/*echo.websocket.events*/} 
+      {/*echo.websocket.events/.ws*/} 
     </div>
   );
 }
