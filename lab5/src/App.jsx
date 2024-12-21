b6d4f2ca36a3c5a867a782ef1dd5a0b8c04f6d72
Slava <vt221_psa@student.ztu.edu.ua>
2024-12-21 13:18:45 +0200

    Integrate SPA into App

--- lab5/src/App.jsx
+++ lab5/src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import AddItem from './components/AddItem';
+import ItemList from './components/ItemList';
 
 const App = () => {
   return (
     <div>
-
+      <h1>Мій SPA Застосунок</h1>
+      <AddItem />
+      <ItemList />
     </div>
   );
 };
